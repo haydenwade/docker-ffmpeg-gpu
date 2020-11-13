51623b6deed21232cac39df061993931aa01c9fe
haydenwade <hayden.wade@alula.net>
2020-11-13 10:11:12 -0600

    switch to file1

--- transcode.js
+++ transcode.js
@@ -44,7 +44,7 @@ const main = async () => {
     try {
         var hrstart = process.hrtime()
 
-        const input = `./input/p1.ps`
+        const input = `./input/file1.ps`
         const output = `./output/${new Date().getTime()}.mp4`;
 
 
