6a4619ac9e36d2eaa3bfc896519dfce3ded8bda3
haydenwade <hayden.wade@alula.net>
2020-11-13 14:59:27 -0600

    added s3 upload and additional test files

--- transcode.js
+++ transcode.js
@@ -1,4 +1,12 @@
 const ffmpeg = require('fluent-ffmpeg');
+const AWS = require('aws-sdk');
+
+AWS.config = {
+    accessKeyId: process.env.AWS_AKI,
+    secretAccessKey: process.env.AWS_SAK,
+    region: 'us-east-1',
+    maxRetries: 3,
+  };
 
 // const ffmpegPath = '/usr/local/app/dependencies/linux/ffmpeg';
 // const ffprobePath = '/usr/local/app/dependencies/linux/ffprobe';
@@ -44,22 +52,25 @@ const main = async () => {
     try {
         var hrstart = process.hrtime()
 
-        const input = `./input/m1.mp4`
+        const input = `./input/6f.mp4`
         const output = `./output/${new Date().getTime()}.mp4`;
 
 
-        //mp4 - from hackathon
         let outputOptions = [
             "-c:a copy",
             "-c:v h264_nvenc",
-            "-b:v 5M"
+            "-b:v 5M",
+            '-profile:v baseline',
+            '-level 3.0',
+            '-movflags +faststart',
+            `-s 960x540`
         ]
 
         let inputOptions = [
             "-vsync 0",
             "-hwaccel cuvid",
             "-c:v h264_cuvid",
-            "-resize 960x540",
+            // "-resize 960x540",
         ]
 
 
@@ -72,12 +83,24 @@ const main = async () => {
                 console.log('end transcode')
                 hrend = process.hrtime(hrstart)
                 console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
+            
+                console.log('uploading')
+                const bodyStream = fs.createReadStream(output);
+                const data = {
+                    Key: `${path}/${fileName}`,
+                    Body: bodyStream,
+                    ContentType: 'video/mp4',
+                    ContentDisposition : 'attachment'
+                  };
+                  await s3.putObject(data).promise();
+                  console.log('done uploading')
             })
             .on('error', (err) => {
                 console.error('error transcoding:', err)
             })
             .save(output);
 
+           
     }
     catch (err) {
         console.log(err);
